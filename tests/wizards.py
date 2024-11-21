54ba204414615702e86d2a16d05d67900d5382ed
tholtmark <tedholtmark@gmail.com>
2024-11-21 21:22:05 +0000

    test script update

---
+++ tests/wizards.py
@@ -0,0 +1,28 @@
+fireball_damage = 500
+potion_mana = 100
+fireball_cost = 50
+
+
+class Wizard:
+    def __init__(self, name, stamina, intelligence):
+        self.name = name
+        self.__stamina = stamina
+        self.__intelligence = intelligence
+        self.mana = self.__intelligence * 10
+        self.health = self.__stamina * 100
+
+    def cast_fireball(self, target):
+        if self.mana < fireball_cost:
+            raise Exception(f"{self.name} cannot cast fireball")
+        else:
+            target.get_fireballed()
+            self.mana -= fireball_cost
+        
+    def is_alive(self):
+        return self.health > 0
+            
+    def get_fireballed(self):
+        self.health -= fireball_damage
+
+    def drink_mana_potion(self):
+        self.mana += potion_mana
